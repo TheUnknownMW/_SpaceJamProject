2a759b5c16a0f0a5fd7e5b26e9d6654f19880778
not nick <nicolaskeil@hotmail.com>
2017-03-15 14:30:48 -0700

    hi

---
+++ proCat1.php
@@ -0,0 +1,59 @@
+<?php
+            require('modules/navbar.php') //Head
+        ?>
+    
+      <div class="row">
+    <h2 style="padding-top: 50px;" >Product Categories</h2>
+        
+        <ul class="accordion" data-accordion>
+  <li class="accordion-item" data-accordion-item>
+    <a href="#" class="accordion-title" style="font-size: 30px;">Space Jam</a>
+    <div class="accordion-content" style="font-size: 20px;" data-tab-content>
+     
+          <a  href="catspace.html" class="button secondary" style="padding:0; ">
+           <img class="" src="images/catagory/CatSpaceJam1.png" alt="SpaceJam1" >
+            <h3 class="text-justify">The Space Jam category includes your most basic Space Jam necessities, from the actual used dvd to the exclusive audio casette tape.</h3>
+        </a>
+    </div>
+  </li>
+  
+  <li class="accordion-item" data-accordion-item>
+    <a href="#" style="font-size: 30px;" class="accordion-title">Accessories</a>
+    <div class="accordion-content" style="font-size: 20px;" data-tab-content>
+         
+          <a  href="catacc.html" class="button secondary" style="padding:0; ">
+            <img class="" src="images/catagory/CatAccessories.png" alt=""> 
+            <h3 class=" text-justify">The Accessories category includes all the extra items you need to be a successful Space Jammer.</h3>
+        </a>
+    </div>
+  </li>
+   
+   <li class="accordion-item" data-accordion-item>
+    <a href="#" class="accordion-title" style="font-size: 30px;">Clothing Items</a>
+    <div class="accordion-content" style="font-size: 20px;" data-tab-content>
+     
+      <a  href="catcloth.html" class="button secondary" style="padding:0; ">
+            <img class="" src="images/catagory/CatClothing.png" alt=""> 
+            <h3 class=" text-justify">The Clothing category outfits you to slam with confidence and style. Our very own founder, Dr. M. Jordan, buys all his attire from this category.</h3>
+        </a>
+    </div>
+  </li>
+   
+   <li class="accordion-item" data-accordion-item>
+    <a href="#" class="accordion-title" style="font-size: 30px;">Home and Decor</a>
+    <div class="accordion-content" style="font-size: 20px;" data-tab-content>
+     
+      <a  href="cathome.html" class="button secondary" style="padding:0; ">
+            <img class="" src="images/catagory/CatHomeDecor.png" alt=""> 
+            <h3 class=" text-justify">Now you can slam on and off the court with our new line of Space Jam accents and furnishings.</h3>
+        </a>
+    </div>
+  </li>
+</ul>
+          </div>
+        <script src="js/vendor/jquery.js"></script>
+    <script src="js/vendor/what-input.js"></script>
+    <script src="js/vendor/foundation.js"></script>
+    <script src="js/app.js"></script>
+  </body>
+</html>
